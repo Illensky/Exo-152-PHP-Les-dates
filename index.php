9401270f1c5539bf2d1dcecf38ab8078d86a9171
Illensky <alexis.laroche.02240@gmail.com>
2022-01-11 15:34:48 +0100

    exercice done

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
 // Premiere ligne
+echo date("l")."<br><br>";
 
+// Deuxieme ligne
+echo date("Y.d.m", 1539302400)."<br><br>";
 
-
-
-
-
+// Troisiéme ligne
+echo date("H:i:s", 1546342507)."<br><br>";
